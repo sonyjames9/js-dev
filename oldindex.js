fd7886ef8a93f06e2a0ca85a99dcda11edf90c09
sj9_y_mac <sonyjames9@yahoo.com>
2023-12-10 21:36:34 +0530

    index

--- oldindex.js
+++ oldindex.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const port = 3000
+const bodyparser = require("body-parser")
 const app = express();
+var jsonparser = bodyparser.json()
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+// app.use(jsonparser)
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+app.post('/', function (req, res) {
+  res.send('Hello World')
+  console.log(req.body)
 })
 
 app.listen(port, function () {
--- todos/index.js
+++ todos/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+
 app.use("/static", express.static("public"));
 
 app.set("view engine", "ejs");
